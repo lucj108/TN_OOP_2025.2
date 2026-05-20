306cae6536d05ebcd185a78ff2af0974e148bf89
Nguyễn Đình Lực <dinhluc2006ht@gmail.com>
2026-05-20 14:25:38 +0700

    Sua lai comment

--- Lab04/AimsProject/src/hust/soict/hedspi/aims/cart/Cart.java
+++ Lab04/AimsProject/src/hust/soict/hedspi/aims/cart/Cart.java
@@ -121,22 +121,9 @@ public class Cart {
     	return null;
     }
     
-   /* public List<Media> filterById(int id) {
+   public List<Media> filterById(int id) {
         List<Media> filteredList = new ArrayList<Media>();
         for (Media m : itemsOrdered) {
-            if (m.getId() == id) {
-                filteredList.add(m);
-            }
-        }
-        return filteredList;
-    }
-    */
-    public List<Media> filterById(int id) {
-        List<Media> filteredList = new ArrayList<Media>();
-        System.out.println("DEBUG: Dang tim kiem ID: " + id); // In ra xem nó nhận ID nào
-        
-        for (Media m : itemsOrdered) {
-            System.out.println("DEBUG: Dang kiem tra Media: " + m.getTitle() + " - ID cua no la: " + m.getId()); // In ra ID của từng món
             if (m.getId() == id) {
                 filteredList.add(m);
             }
